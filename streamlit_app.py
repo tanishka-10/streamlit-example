4ed3d2b7ca60a1a4706fa322e7396ab2f2bc6f0d
tanishka-10 <138995203+tanishka-10@users.noreply.github.com>
2024-02-28 00:06:18 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,11 @@ r.source("./utils.R")
 p=r.my_function()'''
 
 
-
-
+st.subheader('1. Printing text in R')
+with st.expander('See code'):
+  code1 = '''print("Hello world ...")
+  '''
+  st.code(code1, language='R')
 process1 = subprocess.Popen(["Rscript", "PortFolio_Recom.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 result1 = process1.communicate()
 st.write(result1)
