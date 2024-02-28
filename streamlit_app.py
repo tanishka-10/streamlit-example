471d5a7cd8283103918ab430f6b68d4d964d7c20
tanishka-10 <138995203+tanishka-10@users.noreply.github.com>
2024-02-27 23:51:41 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,21 @@ import streamlit as st
 r=ro.r
 r.source("./utils.R")
 p=r.my_function()'''
+
+'''
+import streamlit as st
+import subprocess
+
+
+r=ro.r
+r.source("./utils.R")
+p=r.my_function()
+
+
+
+
+process1 = subprocess.Popen(["Rscript", "PortFolio_Recom.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+result1 = process1.communicate()
+st.write(result1)
+'''
 print("Hello")
